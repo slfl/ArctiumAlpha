81ae9a04798c07ef3e243e04eab80fa53361cd21
Artur <slfl@mail.ru>
2015-05-14 16:59:45 +0300

    Firs commit in AuthServer

---
+++ AuthServer/AuthServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AuthServer
+{
+    class AuthServer
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
